ccf93b81d2e7f2d0c08126cb6f7a419888ad3361
James Keats <james.keats@mymail.champlain.edu>
2018-02-14 12:18:12 -0500

    Start following the node tutorial (again).

---
+++ Assets/NodeDialog/Editor/Node.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace UnityEditor
+{
+    public class Node
+    {
+        private Rect mRect;
+        private string mTitle;
+
+        private GUIStyle mStyle;
+
+	    public Rect rect { get { return mRect; } }
+		public string title { get { return mTitle; } }
+		public GUIStyle style { get { return mStyle; } }
+
+		public Node(Vector2 pos, float width, float height, GUIStyle nodeStyle)
+		{
+			mRect = new Rect(pos.x, pos.y, width, height);
+			mStyle = nodeStyle;
+		}
+
+		public void Drag(Vector2 delta)
+		{
+			mRect.position += delta;
+		}
+
+		public void Draw()
+		{
+			//GUI.Box(rect, title, style);
+			var content = EditorGUIUtility.IconContent("node0 hex");
+			style.normal.background = content.image as Texture2D;
+			
+			GUI.Box(rect, title, style);
+		}
+
+		public bool ProcessEvents(Event e)
+		{
+			return false;
+		}
+	}
+}
--- Assets/NodeDialog/Editor/NodeEditorWindow.cs
+++ Assets/NodeDialog/Editor/NodeEditorWindow.cs
@@ -1,4 +1,5 @@
-﻿using NodeDialog;
+﻿using System.Collections.Generic;
+using NodeDialog;
 using UnityEngine;
 
 namespace UnityEditor
@@ -14,7 +15,20 @@ namespace UnityEditor
             window.titleContent = new GUIContent("Node Dialog Editor");
         }
 
-        private void OnGUI()
+		private List<Node> mNodes;
+		private GUIStyle mNodeStyle;
+
+		private void OnEnable()
+		{
+			mNodeStyle = new GUIStyle
+			{
+				border = new RectOffset(25, 25, 7, 7)
+			};
+			GUIContent content = EditorGUIUtility.IconContent("node0 hex");
+			mNodeStyle.normal.background = content.image as Texture2D;
+		}
+
+		private void OnGUI()
         {
             DrawNodes();
             ProcessEvents(Event.current);
@@ -24,11 +38,38 @@ namespace UnityEditor
         }
 
         private void DrawNodes()
-        {
-        }
+		{
+			if (mNodes == null)
+				return;
+
+			foreach (Node node in mNodes)
+				node.Draw();
+		}
 
         private void ProcessEvents(Event current)
         {
+			switch (current.type)
+			{
+				case EventType.MouseDown:
+					if (current.button == 1)
+						ProcessContextMenu(current.mousePosition);
+					break;
+			}
         }
-    }
+
+		private void ProcessContextMenu(Vector2 mousePosition)
+		{
+			GenericMenu rightClickMenu = new GenericMenu();
+			rightClickMenu.AddItem(new GUIContent("Add node"), false, () => OnClickAddNode(mousePosition));
+			rightClickMenu.ShowAsContext();
+		}
+
+		private void OnClickAddNode(Vector2 mousePosition)
+		{
+			if (mNodes == null)
+				mNodes = new List<Node>();
+
+			mNodes.Add(new Node(mousePosition, 200.0f, 50.0f, mNodeStyle));
+		}
+	}
 }
