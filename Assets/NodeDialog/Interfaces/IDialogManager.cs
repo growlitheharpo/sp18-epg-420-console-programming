b0339c7ac825d5d03574892504552662d18a6c6c
James Keats <james.keats@mymail.champlain.edu>
2018-04-08 21:31:32 -0400

    Add the interfaces from the TDD

---
+++ Assets/NodeDialog/Interfaces/IDialogManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using NodeDialog.Graph;
+
+namespace NodeDialog.Interfaces
+{
+	public interface IDialogManager
+	{
+		void ExecuteToPause(IDialogSpeaker speaker);
+
+		string LocalizeToken(string token);
+	}
+}
---
+++ Assets/NodeDialog/Interfaces/IDialogSpeaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using NodeDialog.Graph;
+
+namespace NodeDialog.Interfaces
+{
+	public interface IDialogSpeaker
+	{
+		void HandleStatement(IDictionary<string, string> userVars, string statementToken);
+
+		void HandlePlayerChoice(IDictionary<string, string> userVars, string statementToken, Action<BaseConnection> onChoiceCompleteCallback);
+	}
+}
